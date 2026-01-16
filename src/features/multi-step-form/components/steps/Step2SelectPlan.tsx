4f81fa8d5253da9861560271486bbf3fbe471274
Rodrigo Rosario Cruz <rodrigorosario435@gmail.com>
2026-01-16 17:26:34 -0400

    feat: added the component Step2SelectPlan

--- src/features/multi-step-form/components/steps/Step2SelectPlan.tsx
+++ src/features/multi-step-form/components/steps/Step2SelectPlan.tsx
@@ -2,7 +2,12 @@ export interface Step2SelectPlanProps {
     prop: string;
 }
 
-export function Step2SelectPlan({ prop }: Step2SelectPlanProps) {
-    
-    return (<div>Step2SelectPlan</div>)
+export function Step2SelectPlan() {
+
+    return (
+        <div>
+            <h1 className="text-blue-950 font-bold text-2xl mb-1">Select Your Plan</h1>
+            <p className="text-grey-500">You have the option of monthly or yearly billing.</p>
+        </div>
+    )
 }
